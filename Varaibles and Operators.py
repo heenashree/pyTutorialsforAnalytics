97ce5a7caaf07a4e38d0ef078fc20d99ccf52a5d
heenashree <heenashree2010@gmail.com>
2019-12-02 15:04:02 +0530

    Initial commit - operators and varaibles

---
+++ Varaibles and Operators.py
@@ -0,0 +1,41 @@
+a = 1
+b = 2
+c = 3.4
+d = True
+f = 'Hello'
+g = 'World'
+
+print(a+b)
+print(a+2)
+print(b+c)
+print(c+2)
+print(f+g)
+
+# Try below
+# print(d+f)
+# print(a+f)
+#print(str(a) + f)
+# type(f)
+# now try print(a+d)
+
+print(2+3)
+print(8/2)
+print(5//2)  # floor division that gives you quotient
+print(10-11)
+print(8+2*3)
+print((8+2)*3)
+print("square", 2*2)
+print("cube", 2*2*2, 2**3)
+print("modules", 8%2, 8%3)
+k = 'Hellow' 'World'
+print(k)
+#k1 = 'Nothing's happening' '
+#print(k1)
+k2 = 'Nothing\'s happeing'
+print(k2)
+k3 = "Nothing's happening"
+print(k3)
+print(k2*2) # print a string 2 times and this only works with strings and not numbers where actual calculation will happen
+print('10' *2)
+print('C:\kpt\nbc')  # new line \n
+print('C:\kpt\\nbc')
