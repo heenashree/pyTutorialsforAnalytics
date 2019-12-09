beb635510671ee6edf27e64af6638666da892758
heenashree <heenashree2010@gmail.com>
2019-12-09 14:55:38 +0530

    loops and lists

---
+++ loops and lists.py
@@ -0,0 +1,19 @@
+array =[ 'a', 'ab', '1','2','3','4']
+for i in array:
+    print(i)
+
+print(len(array))
+for i in range(len(array)):
+    print(i)
+
+
+for i in range(10):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
+
+'''
+k = [i for i in array if i.isdigit()]
+print(k)
+'''
