04d726c84185cd5103cf118ec15b7b420476f2b3
heenashree <heenashree2010@gmail.com>
2019-12-30 14:50:42 +0530

    file operations

--- file operations.py
+++ file operations.py
@@ -8,16 +8,30 @@ f = open('templates\dummy1.json', 'a') #'w' will create a new file if it doesnt 
 f.write(testdict)
 f.close()
 
-testyaml = 'Hello there'
+testyaml = 'Hello there\nHow is it going'
 with open('templates\dummy2.yml', 'w') as file:
     file.write(testyaml)
 '''
-“ r “, for reading.
-“ w “, for writing.
-“ a “, for appending.
-“ r+ “, for both reading and writing
+'r'	Open a file for reading. (default)
+'w'	Open a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
+'x'	Open a file for exclusive creation. If the file already exists, the operation fails.
+'a'	Open for appending at the end of the file without truncating it. Creates a new file if it does not exist.
+'t'	Open in text mode. (default)
+'b'	Open in binary mode.
+'+'	Open a file for updating (reading and writing)
 '''
+with open('templates\dummy2.yml', 'r') as file:
+    k = file.read(1)
+    print(k)
+    file.seek(0)
 
+    print(file.readline())
+    print(file.readline())
+    file.seek(0)
+
+    print(file.readlines())
+
+#https://www.programiz.com/python-programming/file-operation
 
 
 
--- file_operation2.py
+++ file_operation2.py
@@ -11,5 +11,13 @@ for k, v in list_data.items():
 dict_file = [{'sports' : ['soccer', 'football', 'basketball', 'cricket', 'hockey', 'table tennis']},
 {'countries' : ['Pakistan', 'USA', 'India', 'China', 'Germany', 'France', 'Spain']}]
 
-with open('templates\yamldata.yml', 'a') as f:
+with open('templates\yamldata1.yml', 'a') as f:
     yaml.dump(dict_file, f)
+
+
+## remove the file
+import os
+if os.path.exists("templates\demofile.txt"):
+  os.remove("templates\demofile.txt")
+else:
+  print("The file does not exist")
