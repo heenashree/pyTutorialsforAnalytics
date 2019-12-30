1c74b491efe0cbed2dbb5d531bc0d99390e58a86
heenashree <heenashree2010@gmail.com>
2019-12-30 14:13:34 +0530

    file operations

--- file operations.py
+++ file operations.py
@@ -1,3 +1,23 @@
 import json
 import yaml
 
+testdict = 'hello 2020'
+
+## Create files in python
+f = open('templates\jsondata.json', 'a') #'w' will create a new file if it doesnt exist
+f.write(testdict)
+f.close()
+
+testyaml = 'Hello there'
+with open('templates\yamldata.yml', 'w') as file:
+    file.write(testyaml)
+'''
+“ r “, for reading.
+“ w “, for writing.
+“ a “, for appending.
+“ r+ “, for both reading and writing
+'''
+
+
+
+
